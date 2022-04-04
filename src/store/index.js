d33190ab74e6f513a6d36168d92a46df1cb94c00
MatAndreoli <andreoli.2003@gmail.com>
2022-04-04 10:57:43 -0300

    [VBTB-5833][VBTB-5842] Created PokemonItem component

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: [],
+    detail: {},
   },
   getters: {
+    getList(state) {
+      return state.list;
+    },
+    getDetail(state) {
+      return state.detail;
+    },
   },
   mutations: {
+    ADD_TO_LIST(state, payload) {
+      state.list = payload;
+    },
+    ADD_TO_DETAIL(state, payload) {
+      const pokemonDetail = state.list.find((pokemon) => payload == pokemon.id);
+      if (pokemonDetail) {
+        state.detail = pokemonDetail;
+      }
+    },
   },
   actions: {
+    async getPokemonList(context) {
+      axios.get(`http://localhost:8080/pokemons/`).then((res) => {
+        context.commit('ADD_TO_LIST', res.data);
+      });
+    },
   },
-  modules: {
-  }
 })
---
+++ tests/unit/PokemonItem.vue.spec.js
@@ -0,0 +1,32 @@
+import PokemonItem from '../../src/components/PokemonItem.vue';
+import { shallowMount } from '@vue/test-utils';
+
+const factory = (propsData) =>
+  shallowMount(PokemonItem, {
+    propsData: {
+      ...propsData,
+    },
+  });
+
+describe('PokemonItem', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = factory({img: 'url', name: 'matheus'});
+  });
+  
+  it('should validate name', () => {
+    expect(PokemonItem.name).toEqual('PokemonItem');
+  });
+
+  it('should have components', () => {
+    const h2Text = wrapper.find('h2').element.textContent;
+    expect(wrapper.find('img').exists()).toBe(true);
+    expect(h2Text).toContain('Matheus');
+  });
+
+  it('when clicked should emit pokemonEvent then emit an empty array', () => {
+    const eventClick = wrapper.find('.cards').trigger('click')
+    expect(eventClick).not.toBeUndefined();
+    expect(wrapper.emitted('pokemonEvent')).not.toBeUndefined()
+  });
+});
