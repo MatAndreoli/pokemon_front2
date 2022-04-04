7325f80ff79e0706ce731ce145cb42ef579c3935
MatAndreoli <andreoli.2003@gmail.com>
2022-04-04 14:09:03 -0300

    [VBTB-5833][VBTB-5842] Refactored test

--- tests/unit/PokemonItem.vue.spec.js
+++ tests/unit/PokemonItem.vue.spec.js
@@ -18,15 +18,13 @@ describe('PokemonItem', () => {
     expect(PokemonItem.name).toEqual('PokemonItem');
   });
 
-  it('should have components', () => {
+  it('should have the value of prop name in the <h2></h2>', () => {
     const h2Text = wrapper.find('h2').element.textContent;
-    expect(wrapper.find('img').exists()).toBe(true);
     expect(h2Text).toContain('Matheus');
   });
 
   it('when clicked should emit pokemonEvent then emit an empty array', () => {
-    const eventClick = wrapper.find('.cards').trigger('click')
-    expect(eventClick).not.toBeUndefined();
+    wrapper.find('.cards').trigger('click')
     expect(wrapper.emitted('pokemonEvent')).toHaveLength(1)
   });
 });
