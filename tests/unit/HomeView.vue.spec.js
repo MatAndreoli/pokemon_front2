3dbb10290d71885755673dd7146c4d519e2fa6f5
MatAndreoli <andreoli.2003@gmail.com>
2022-04-11 09:09:28 -0300

    [VBTB-5949][VBTB-5951] Moved vue project to client/

--- tests/unit/HomeView.vue.spec.js
+++
@@ -1,9 +0,0 @@
-import HomeView from '../../src/views/HomeView.vue';
-import { shallowMount } from '@vue/test-utils';
-
-describe('HomeView', () => {
-  it('HomeView component should exist', () => {
-    shallowMount(HomeView);
-    expect(HomeView.name).toBe('HomeView');
-  });
-});
