85681b144ae09fc1f0243129f66b08155e3f48ee
MatAndreoli <andreoli.2003@gmail.com>
2022-05-06 16:28:53 -0300

    Update package.json

--- client/tests/unit/gateways/pokemon_api.spec.js
+++ client/tests/unit/gateways/pokemon_api.spec.js
@@ -12,7 +12,7 @@ describe('pokemon_api', () => {
     });
 
     it('then should call http.get with expected params', () => {
-      expect(http.get).toHaveBeenCalledWith('http://localhost:3000/pokemons/1');
+      expect(http.get).toHaveBeenCalledWith('/1');
     });
   });
 });
